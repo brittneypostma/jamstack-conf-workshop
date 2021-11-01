399019914e9f52071667158635f343299fe91779
brittneypostma <brittney.postma@hotmail.com>
2021-11-01 14:17:04 -0400

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+{JSON.stringify(posts)}
+
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch('/posts.json');
+		if (res.ok) {
+			const posts = await res.json();
+		}
+		return {
+			props: {
+				posts
+			}
+		};
+	}
+</script>
+
+<script>
+	export let posts;
+</script>
